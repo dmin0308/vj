69018c9495c1682a6203160ff93fcd24706a8d1f
dmin0308 <sonja4485@gmail.com>
2024-09-24 12:51:21 +0900

    ``와 $를 사용하여 변수를 넣어 사용하기

--- object/letconst.js
+++ object/letconst.js
@@ -1,18 +1,22 @@
 const family = {
-  d1text : "패밀리사이트", 
-  familysite : [
-    "네이버","구글","다음","유튜브","끼야우"
-  ] 
-}
+  d1text: "Realativesite",
+  familysite: [
+    {
+      atext: "git",
+      href: "https://github.com/dmin0308",
+    },
+    {
+      atext: "설화수",
+      href: "https://dmin-project-test.notion.site/ccba52a8ff4542aa9e39856a6cd22c72?v=d4da144b490c4cf5b953f01020014903&pvs=4",
+    },
+    {
+      atext: "피그마",
+      href: "https://www.figma.com/design/Aw7jOFG239btKXJsnBIFYx/project_sulwhasoo?node-id=0-1&t=wiU0mNpZG4hRJEWf-1",
+    },
+  ],
+};
+console.log(family.familysite[1].atext, family.familysite[1].href)
 
-// console.log(family.familysite[0])
+const mytag = `<a href="${family.familysite[1].href}">${family.familysite[1].atext}</a>`;
 
-for(let x in family.familysite){
-  //index  familysite의 자리 0,1,2,3,4
-  console.log(x)
-}
-
-for(let x of family.familysite){
-  //value familtsite의 값 "네이버","구글","다음","유튜브","끼야우"
-  console.log(x)
-}
+console.log(mytag);
