0214f2b6b2967fb84e45d8dda0a9fecfc2411af6
dmin0308 <sonja4485@gmail.com>
2024-09-25 13:05:35 +0900

    최종본(자동화 최적화_증감식이용)

--- object/letconst.js
+++ object/letconst.js
@@ -17,19 +17,17 @@ const family = {
 };
 
 let familytag='';
+familytag += `<h2>${family.d1text}</h2><ul>`;
+
 
 for(x of family.familysite ){
   familytag +=`<li>${x.atext}</li>`
-
 }
-
+familytag +=`</ul>`;
 
 window.addEventListener('load', function(){
 
-  document.querySelector("#family h2")
-  .innerHTML = family.d1text;
-
-  document.querySelector("#family ul")
+  document.querySelector("#family")
   .innerHTML = familytag;
 
 })
