5118b80e6fda026d84487a35e4e1b3358b97bd7b
dmin0308 <sonja4485@gmail.com>
2024-09-26 11:01:04 +0900

    for in/of 단순화 작업

--- src/basic.js
+++ src/basic.js
@@ -2,23 +2,16 @@ window.addEventListener("load", function () {
   //호출영역
   this.document.querySelector("#family h2").innerHTML = family.d1text;
 
-  const mylist = this.document.querySelectorAll("#family li");
+  let familylist = "";
 
-  console.log(
-    `정체파악 : ${mylist}`, // [object, object, object]
-    `데이터타입 : ${typeof mylist}`,
-    // object -> array도 object에 해당됨
-    `배열인가? ${Array.isArray(mylist)}`
-    // 배열인지 아닌지 확인하기 true, false로 출력됨
-  );
-  
-  mylist[0].innerHTML = family.familysite[0].atext;
+  // for(x in family.familysite){
+  // familylist += `<li>${family.familysite[x].atext}</li>`;}
 
-  for(0 in mylist){
-    mylist[0].innerHTML = family.familysite[0].atext;
+  for (x of family.familysite) {
+    familylist += `<li>${x.atext}</li>`;
   }
 
-
+  this.document.querySelector("#family ul").innerHTML = familylist;
 });
 
 //함수 제작
