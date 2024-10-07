bb3091e5f9c74ed7689445595bc3fa4f3f38b04e
dmin0308 <sonja4485@gmail.com>
2024-10-07 11:40:50 +0900

    배경색에 맞게 글자색 변환 자바스크립트 활용(슬라이드 지정)

--- src/layout/js/page_main.js
+++ src/layout/js/page_main.js
@@ -1,15 +1,33 @@
-window.addEventListener('load', function(){
+window.addEventListener("load", function () {
+  const mainBanner = new Swiper("#mainBanner .swiper-container", {
+    effect: "fade",
+    slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
+    spaceBetween: 0, // 슬라이드 간의 간격
+    // centeredSlides: true,      // 가운데 정렬 모드
+    loop: true, // 반복 처리
+    autoplay: {
+      delay: 6000, // 자동 롤링 딜레이 (3초)
+      disableOnInteraction: false, // 사용자 상호작용 후에도 자동 롤링 유지
+    },
+    on: {
+      init: function () {
+        
 
-const mainBanner = new Swiper('#mainBanner .swiper-container', {
-  effect: "fade",
-  slidesPerView: 1,          // 한 번에 보여줄 슬라이드 개수
-  spaceBetween: 0,          // 슬라이드 간의 간격
-  //centeredSlides: true,      // 가운데 정렬 모드
-  loop: true,                // 반복 처리
-  autoplay: {
-    delay: 5000,             // 자동 롤링 딜레이 (3초)
-    disableOnInteraction: false,  // 사용자 상호작용 후에도 자동 롤링 유지
-  },
-});
+      },
+      realIndexChange: function () {
+        // 실제슬라이드가 변경되면
+        const activeIndex = this.activeIndex; // 현재 활성화된 슬라이드의 인덱스
+        const realIndex = this.realIndex; // 실제 슬라이드의 인덱스
+        console.log(activeIndex, realIndex);
 
+        const classesToRemove = Array.from(document.body.classList).filter(
+          (className) => className.startsWith("slide-"));
+          //array 메서드 filter를 통해서 해당 조건의 값만 필터링 하므로 데이터 타입은 여전히 배열
+        document.body.classList.remove(...classesToRemove);
+          //배열 [ ] 기호를 제외함
+
+        document.body.classList.add(`slide-${realIndex}`); // 새로운 클래스 추가
+      },
+    },
+  });
 });
