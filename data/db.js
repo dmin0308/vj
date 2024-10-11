f944a2ddeec32148bc40b0b57aa026bd9ec66bee
dmin0308 <sonja4485@gmail.com>
2024-10-11 11:36:19 +0900

    faq ver.1

--- data/db.js
+++ data/db.js
@@ -36,36 +36,36 @@ subMenu: [
 },
 {
 title: {
-  text: "네이버",
-  link: "https://www.naver.com/"
-},
-subMenu: [
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" }
-]
-},
-{
-title: {
-  text: "유튜브",
-  link: "https://www.youtube.com/"
-},
-subMenu: [
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" },
-  { text: "소메뉴", link: "" }
-]
-},
-{
-title: {
-  text: "포트폴리오",
+  text: "작품",
   link: "#portfolio"
+},
+subMenu: [
+  { text: "찾아오시는 길처럼 긴소메뉴", link: "" },
+  { text: "소메뉴", link: "" },
+  { text: "소메뉴", link: "" },
+  { text: "소메뉴", link: "" },
+  { text: "소메뉴", link: "" },
+  { text: "소메뉴", link: "" },
+  { text: "소메뉴", link: "" }
+]
+},
+{
+title: {
+  text: "유투브",
+  link: "http://www.youtube.com"
+},
+subMenu: [
+  { text: "소메뉴", link: "" },
+  { text: "소메뉴", link: "" },
+  { text: "소메뉴", link: "" },
+  { text: "소메뉴", link: "" },
+  { text: "소메뉴", link: "" }
+]
+},
+{
+title: {
+  text: "사전인터뷰",
+  link: "#faqcontent"
 },
 subMenu: [
   { text: "소메뉴", link: "" },
@@ -93,6 +93,22 @@ subMenu: [
 //setTimeout 
 const swiperBanner = [
 
+]
+
+
+const faqcontent = [
+{
+faqQ : "프론트앤드 과정을 훈련받으면서 가장 어려웠던 점은?",
+faqA : "K 디지털훈련에서 팀프로젝트를 완수해야하는데 훈련생으로써 ....."
+},
+{
+faqQ : "훈련과정이 GPT를 활용하는 타이틀인데 실무에 영향이...",
+faqA : "일단 나의 목적이 뚜렷하고 그것을 이루기 위한 질문..."
+},
+{
+faqQ : "귀사입사후 어떤 업무들이 가능하신지 구체적으로",
+faqA : "클라이언트와의 요구를 반영한 UI작업이 가능하며 |  컴포넌트 및 라이브러리를 적극활용하여 빠른 제작이 가능 | 디지털소스를 깃허브에 업로드하여 공유화하여 다른 포지션들과의 원활한 의사소통이 가능하며... "
+}
 ]
 
 
--- src/api/navi.js
+++ src/api/navi.js
@@ -1,42 +1,77 @@
-window.addEventListener('load', function(){
-
+window.addEventListener("load", function () {
   let navitag = ""; // 값변경가능한 변수를 선언
-  const navitarget = this.document.querySelector("#navi >ul"); 
+  const navitarget = this.document.querySelector("#navi >ul");
 
-   for(x of mynavi){
+  for (x of mynavi) {
     navitag += `<li class="position-relative">
                 <a href="${x.title.link}" class="d-block px-lg-5">${x.title.text}</a>
                 <div class="position-absolute">
                   <ul>`;
-         for( i of x.subMenu){
-                    navitag += `<li><a href="${i.link}" class="text-nowrap">${i.text}</a></li>`;
-         }                   
+    for (i of x.subMenu) {
+      navitag += `<li><a href="${i.link}" class="text-nowrap">${i.text}</a></li>`;
+    }
     navitag += ` </ul>
                 </div>
-              </li>`
-             }
-             navitarget.innerHTML = navitag;
-
+              </li>`;
+  }
+  navitarget.innerHTML = navitag;
 
   //호출영역
   this.document.querySelector("#family h2").innerHTML = family.d1text;
 
-  let familylist  = '';
+  let familylist = "";
 
-  // for(x in family.faimilysite ){
-  //     familylist += `<li>${family.faimilysite[x].atext}</li>`;
-  // }
-
-  for(x of family.faimilysite ){
+  for (x of family.faimilysite) {
     familylist += `<li>${x.atext}</li>`;
   }
   this.document.querySelector("#family ul").innerHTML = familylist;
 
+  //faq
+
+  let faqtag = ""; // 값변경가능한 변수를 선언
+  const faqtarget = this.document.querySelector("#faqcontent  .content");
+
+  faqtag += `<dl class="container py-5">`;
+
+  for (x of faqcontent) {
+    faqtag += `<dt class="border-bottom py-3 d-flex justify-content-between">
+    <span>${x.faqQ}</span> 
+    <i class="bi bi-chevron-down"></i>
+    </dt>
+                   <dd class="py-4 d-none">`;
+    const faqcontentArr = x.faqA.split("|");
+    for (j of faqcontentArr) {
+      faqtag += `<span class='d-block'> ${j} </span>`;
+    }
+    faqtag += `</dd>`;
+  }
+
+  faqtag += `</dl>`;
+  faqtarget.innerHTML = faqtag;
 
   //패밀리노출
-  this.document.querySelector("#family h2").addEventListener('click', function(){
-    this.parentNode.classList.toggle('open');
-  })
-})
+  this.document
+    .querySelector("#family h2")
+    .addEventListener("click", function () {
+      this.parentNode.classList.toggle("open");
+    });
 
+  //동적객체 = 이벤트에 저장하는 식 불가
+  const faqdts = document.querySelectorAll("#faqcontent  .content dt");
+  //사전 인터뷰 dt들 저장
+
+  faqdts.forEach((ele, idx) => {
+    ele.addEventListener('click', function () {
+      
+      faqdts.forEach(function (sibling) {
+       
+        if (sibling == ele) {
+          ele.classList.toggle("expand");
+        } else {
+          sibling.classList.remove("expand");
+        }
+      });
+    });
+  });
+});
 // 함수제작
